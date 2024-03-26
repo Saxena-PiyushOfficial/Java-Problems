21910d0cf2faff72cfc14effb9b4f9dca837eeae
Piyush0831 <147948037+Saxena-PiyushOfficial@users.noreply.github.com>
2024-03-26 14:51:21 +0530

    Add files via upload

---
+++ IPattern06.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class IPattern06 {
+    public static void main(String[] args) {
+        int rows = 5;
+        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <=rows - i ; j++) {
+                System.out.print(" "); // Print spaces for alignment
+            }
+            for (int k = 1; k <= i * 2-1; k++) {
+                System.out.print("*"); // Print asterisks for the pyramid
+            }
+            System.out.println(); // Move to the next line for the next row
+        }
+    }
+}
